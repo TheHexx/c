0d73975c01b60164d84b902eb07921f24db4a835
Andy Penrose <apenrose@apenrose.com>
2017-12-16 22:52:46 +0000

    hello world

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(int argc, char* argv[]) {
+  if(argc == 2)
+  {
+    printf("Hello %s \n", argv[1]);
+  }
+  else 
+  {
+    printf("Usage : main <name>\n");
+  }
+}
